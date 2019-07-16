f90a0c53b77cf775267669707a433598edbbae4c
climentea <climente.alin@gmail.com>
2019-07-16 11:37:09 +0300

    fmk

--- es6_modules/components/btn.js
+++
@@ -1,7 +0,0 @@
-export { button };
-
-const button = () => {
-  let btn = document.createElement("BUTTON"); // Create a <button> element
-  btn.innerHTML = "CLICK ME"; // Insert text
-  document.body.appendChild(btn); // Append <button> to <body>
-};
